91d56d77030534c941ff46d36fe73484785c3f18
takumi-0506 <s2221189@gmail.com>
2025-12-03 15:59:03 +0900

    モンスターの攻撃力を下げた

--- src/App.vue
+++ src/App.vue
@@ -1422,7 +1422,7 @@ const createMonsterAndStartBattle = () => {
       id: index,
       hp: 10,
       maxHp: 10,
-      attack: 10,
+      attack: 5,
       defense: 2,
       DEX: 90,
       evasion: 10,
